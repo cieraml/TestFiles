3b55fe179075749c410794a3e72a355a5fc13440
cieraml <c00034347@louisiana.edu>
2019-09-18 15:18:10 -0500

    operators session :)

---
+++ Operators.R
@@ -0,0 +1,34 @@
+# operators
+a <- (5:15)
+a [(c(1,7))]
+b <- 5
+c <- 11
+b > c
+b < c
+b >= c
+a [c(2,6,9)]
+d <- 6
+f <- 10
+g <- 13
+((d + f) * (d + g)) / 2
+10 * (f - g)
+# the operator for not is NULL
+b %% c
+# %% is for integer division
+b^c
+# ^ is getting the square of a number
+c %/% b
+# %/% is getting the remainder of the divsion 
+55 -> zz
+q <- 34
+r <- 44
+s <- 54
+v <- c(q,r,s)
+# Inf is a non-zero number divided by zero
+k <- 1/0
+# - Inf is a negative non-zero number divided by zero
+h <- -1/0
+# NaN is basically not a number
+0 / 0
+# Na means not available
+is.na(c(1,5))
